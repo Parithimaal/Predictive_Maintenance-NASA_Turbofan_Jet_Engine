43ed2d15968ac1ab84d3ac006b43b2eb7fa70238
Parithimaal <balajiadithya2000@gmail.com>
2025-03-16 23:29:42 -0400

    replace all files

---
+++ FD001 Analysis/preprocess.py
@@ -0,0 +1,135 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.express as px
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import StandardScaler
+
+class Preprocessor:
+    """
+    Contains the functions for preprocessing the NASA Turbofan dataframes
+    """
+    def __init__(self, saved_min_max=MinMaxScaler(), saved_std=StandardScaler()):
+        self.minmaxscaler = saved_min_max
+        self.stdscaler = saved_std
+        self.sensor_cols = []
+        self.op_setting_cols = []
+        self.other_cols = []
+
+    def rename_columns(self, df):# Change in main
+        self.sensor_cols = [f"s{num}" for num in range(1, 24)]
+        self.op_setting_cols = [f"ops{num}" for num in range(1, 4)]
+        self.other_cols = ["unit", "cycles"]
+
+        column_names = self.other_cols + self.op_setting_cols + self.sensor_cols
+        df.columns = column_names
+        return df
+    
+    def get_sensor_cols(self):
+        return self.sensor_cols
+    
+    def get_op_setting_cols(self):
+        return self.op_setting_cols
+    
+    def get_other_cols(self):
+        return self.other_cols
+    
+    @staticmethod
+    def dropna(df):
+        df = df.dropna(axis=1, how='all')
+        return df
+
+    
+    @staticmethod
+    def drop_no_info_cols(df):
+        no_info_cols = []
+        for col in df.describe().columns:
+            if df.describe().loc['min', col] == df.describe().loc['max', col]:
+                no_info_cols.append(col)
+        return df.drop(columns=no_info_cols)
+    
+    def create_rul_col(self, df):# Change in main
+        rul_col = df.groupby('unit')['cycles'].transform('max') - df['cycles']
+        df.insert(loc=2, column='rul', value=rul_col)
+        return df
+
+    def drop_unit(self,df):
+        df = df.drop(columns=['unit'])
+        return df
+
+    def update_col_groups(self, df):
+        self.sensor_cols = [col for col in df.columns if col.startswith('s')]
+        self.op_setting_cols = [col for col in df.columns if col.startswith('ops')]
+        self.other_cols = [col for col in df.columns if col not in self.sensor_cols+self.op_setting_cols]
+        return df
+    
+    def normalize_predictors(self, df, cols):
+        df_norm = df.copy()
+        df_norm[cols] = self.minmaxscaler.fit_transform(df_norm[(cols)])
+        return df_norm
+
+    def standardize_predictors(self, df, cols):
+        df_std = df.copy()
+        df_std[cols] = self.stdscaler.fit_transform(df_std[(cols)])
+        return df_std
+    
+    def get_minmaxscaler(self):
+        return self.minmaxscaler
+    
+    def get_stdscaler(self):
+        return self.stdscaler
+    
+    @staticmethod
+    def group_by_rul(df, cols):
+        agg_dict = {
+            item: 'mean'
+            for item in cols
+        }
+        agg_dict['unit'] = 'count'
+        agg_dict['cycles'] = 'mean'
+        return df.groupby('rul').agg(agg_dict)
+    
+    @staticmethod
+    def group_by_unit(df, cols):
+        agg_dict = {
+            item: 'mean'
+            for item in cols
+        }
+        agg_dict['cycles'] = 'mean'
+        return df.groupby('unit').agg(agg_dict)
+    
+class Charting:
+    
+    @staticmethod
+    def plot_avg_sensor_data(grouped_train_norm, sensor_cols):
+        y_columns = sensor_cols
+        return px.line(grouped_train_norm, x=grouped_train_norm.index, y=y_columns)
+        
+
+    @staticmethod
+    def plot_corr_heatmap(grouped_df):
+        plt.figure(figsize=(12, 10))
+        sns.heatmap(np.round(grouped_df.reset_index().corr(), 2), annot=True, cmap='coolwarm')
+        plt.title('Seaborn Correlation Matrix')
+        return plt
+    
+    @staticmethod
+    def plot_max_cycles_dist(train_df):
+        max_cycles = train_df.groupby('unit').max('cycles').sort_values(by='cycles', ascending=True).reset_index().cycles
+        percentiles = [25, 50, 75]  # 25th, 50th (median), and 75th percentiles
+        percentile_values = np.percentile(max_cycles, percentiles)
+        fig = px.histogram(max_cycles)
+        colors = ['red', 'green', 'purple']
+        line_names = ['p25', 'p50', 'p75']
+        for i, percentile in enumerate(percentile_values):
+            fig.add_vline(
+                x=percentile,
+                line_color=colors[i],
+                line_dash='dash',
+                line_width=2,
+                annotation_text=f"{line_names[i]}",
+                annotation_position="top"
+            )
+        return fig
+    
--- README.md
+++ README.md
@@ -1,1 +1,102 @@
-# Predictive_Maintenance-NASA_Turbofan_Jet_Engine
+# Predictive Maintenance of NASA Turbofan Jet Engine
+
+## Overview
+
+This project focuses on developing predictive maintenance models for NASA's Turbofan Jet Engines. By analyzing engine sensor data, the goal is to predict the Remaining Useful Life (RUL) of each engine, enabling proactive maintenance and reducing downtime.
+
+## Dataset Description
+
+The project utilizes the [NASA Turbofan Engine Degradation Simulation Data Set](https://data.nasa.gov/Aerospace/CMAPSS-Jet-Engine-Simulated-Data/ff5v-kuh6). This dataset comprises multiple multivariate time series from different engine units, each starting with varying degrees of initial wear and manufacturing variation. Each engine operates normally at the start and develops a fault over time.
+
+Key characteristics of the dataset include:
+- Multiple operational settings and sensor measurements.
+- Data divided into training and test sets.
+- The objective is to predict the RUL for each engine in the test set. There are 100 different engine units.
+
+## Folder structure
+```
+Predictive_Maintenance-NASA_Turbofan_Jet_Engine/
+│
+├── data/
+│   └── processed/
+│
+├── FD001 Analysis/
+│   ├── Main.ipynb
+|   ├── Testing.ipynb
+|   ├── saved_models/
+|   ├── saved_scalers/
+|   ├── preprocess.py
+│
+├── Deep Learning/
+│   ├── DL_Main.ipynb
+|   ├── neuralnetworks.py
+|
+├── README.md
+├── requirements.txt
+```
+
+## Data Preprocessing
+
+Data preprocessing steps include:
+
+- Columns were renamed and empty columns were removed
+- New Column (RUL = Max Cycle - Current cycle) was created
+- Normalizing sensor measurements, Operating settings and Cycles.
+
+## Exploratory Data Analysis (EDA)
+
+EDA involves:
+
+- Visualizing sensor trends over time
+- Identifying correlations between sensors and RUL
+- Grouping and observing average sensor values
+
+## Feature Engineering
+
+Features engineered for modeling:
+
+- Clipping was done to contain extreme values
+- Undersampling was done randomly to rarer values
+- Trend features capturing the slope of sensor signals
+- PCA was performed, but yielded poor results
+
+## Modeling Approach
+
+The following predictive models are employed:
+
+- **Random Forest Regressor:** To capture non-linear relationships and interactions between features.
+- **XGBoost:** To handle unbalanced data
+
+GridSearchCV was performed to optimize hyperparameters. Validation set was split from the training data file.
+
+## Model Evaluation
+
+Models are evaluated using:
+
+- **Root Mean Squared Error (RMSE):** Measures the average magnitude of prediction errors.
+- **Mean Absolute Error (MAE):** Provides the average absolute difference between predicted and actual RUL.
+
+## How to Use This Repository
+
+To replicate the analysis:
+
+1. Clone the repository:
+```
+    git clone https://github.com/Parithimaal/Predictive_Maintenance-NASA_Turbofan_Jet_Engine.git
+```
+2. Install required packages
+```
+    pip install -r requirements.txt
+```
+3. For training and tweaking run Main.ipynb
+4. The Testing.ipynb contains the evaluation of saved models from Main.ipynb
+
+## Results and Discussion
+The XGBoost model was the best performer with the an RMSE of 37.25. Interestingly, performing PCA with components responsible for 96% of variance yielded much poorer results. Though the performance of the models is appreciable, it could be improved by providing the models context of the temporal series and past data for each unit.
+
+## Future Work
+- **Deep Learning:** Using Neural Networks such as LSTM and GRU to capture temporal patterns
+
+## References
+[NASA Turbofan Engine Degradation Simulation Data Set](https://data.nasa.gov/Aerospace/CMAPSS-Jet-Engine-Simulated-Data/ff5v-kuh6/about_data)
+
